bacc676d1a647489fc5a79ce0aaaebc0475529cf
Jordan Davis <jordandavis919@gmail.com>
2022-11-14 22:23:11 +0000

    first commit

--- projects/matchy/data.js
+++ projects/matchy/data.js
@@ -11,21 +11,44 @@
 //////////////////////////////////////////////////////////////////////
 // Step 1 - Object Creation //////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
-
-
+//creating empty object
+var animal = {};
+//using dot notation adding key value pair 
+animal.species = "frog";
+//using bracket notation add key value pair
+animal['name'] = 'Frogger';
+//using any notation add key value pair = to empty array
+animal.noises = [];
+//print animal obj
+console.log(animal);
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Array Creation ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
-
-
+//creating empty array
+var noises = [];
+//use bracket notation to give noises its first element
+noises[0] = 'bonk';
+//using an array function to add an element
+noises.push('ribbit');
+//adding element to beginning of array
+noises.unshift('chirp');
+//adding another element with bracket notation
+noises[noises.length] = 'splash';
+//console.log length of array
+console.log(noises.length);
+//console.log last elemenet in array
+console.log(noises[noises.length - 1]);
+//console.log the whole array
+console.log(noises);
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Combining Step 1 and 2 ///////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
-
-
+//assigning the noises prop to our noises array
+animal['noises'] = noises;
+//using any syntax to add another noise
+animal.noises.push("whirrr");
+//logging animals array
+console.log(animal);
 /* *******************************************************************
  * Step 4 - Review
  *
@@ -48,15 +71,49 @@
 //////////////////////////////////////////////////////////////////////
 // Step 6 - A Collection of Animals //////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
-
-
+//creating an empty array
+var animals = [];
+//pushing animal to animals
+animals.push(animal);
+//creating a variable called duck with obj
+var duck = { 
+ species: 'duck',
+ name: 'Jerome',
+ noises: ['quack', 'honk', 'sneeze', 'woosh'] 
+};
+ //push duck to animals 
+ animals.push(duck);
+ //console.log
+ console.log(animals);
+//create 2 more animals and push them to animals 
+var dog = {
+  species: 'dog',
+  name: 'Scooby',
+  noises: ['ruff', 'grrr']
+};
+animals.push(dog);
+var cat = {
+  species: 'cat',
+  name: 'Midnight',
+  noises: ['meow', 'hiss']
+};
+animals.push(cat);
 //////////////////////////////////////////////////////////////////////
 // Step 7 - Making Friends ///////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
-
-
+//creating empty array
+var friends = [];
+//creating a function that will use Math.random to get random and Math.floor 
+function getRandom(index){
+return Math.floor(Math.random() * animals.length)
+}
+//pushing random animal + animal name to friends
+friends.push(animals[getRandom()].name);
+//logging friends
+console.log(friends);
+//adding friends to cat & dog
+cat['friends'] = friends;
+dog['friends'] = friends;
 /**
  * Nice work! You're done Part 1. Pat yourself on the back and
  * move onto Part 2 in the file called "functions.js"
