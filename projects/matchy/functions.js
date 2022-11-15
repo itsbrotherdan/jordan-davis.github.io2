e31196893d82a2685e3642f2cf98046c20bd3e9c
Jordan Davis <jordandavis919@gmail.com>
2022-11-15 20:19:08 +0000

    matchy

--- projects/matchy/functions.js
+++ projects/matchy/functions.js
@@ -12,25 +12,69 @@
 //////////////////////////////////////////////////////////////////////
 // Step 1 - Search ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+//creating a function called search
+function search(animals, name){
+    //searching through animals array and return the value in animals
+for (var i = 0; i < animals.length; i ++){
+    //if animals value === name return animals value
+    if(animals[i].name === name){
+    return animals[i];
+    }
+}
+//returning null if no animal exist
+return null;
+}
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Replace //////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+//creating replace function
+function replace(animals, name, replacement){
+    //iterating through animals 
+for (var i = 0; i < animals.length; i ++){
+    //checking if animals[i] value === name
+    if (animals[i].name === name){
+        //replacing animals value with replacement obj
+        animals[i] = replacement
+    }
+}
+}
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Remove ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+//creating remove function
+function remove(animals, name){
+//iterating of animals 
+for (var i = 0; i < animals.length; i++){
+    if (animals[i].name === name){
+        animals[i] = remove
+    }
+}
+}
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 4 - Add ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+//creating function add
+function add(animals, animal){ 
+    //checking if animal name are greater than 0
+    if (animal.name.length > 0 && animal.species.length > 0){
+        //if they are iterate over animals array
+        for (let i = 0; i < animals.length; i++){
+            //then if animals[i].name is === to animal name 
+            if (animals[i].name === animal.name){
+                //we return
+                return;
+            }
+        }
+        //pushing animal to animals object
+        animals.push(animal);
+    }
+}
 
 
 /**
